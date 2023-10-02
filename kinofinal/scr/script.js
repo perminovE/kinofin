9bdd470742a84b4ce5a25f65ea0fcd70cdf67e05
perminovE <145645169+perminovE@users.noreply.github.com>
2023-10-02 16:41:30 +0500

    Add files via upload

---
+++ kinofinal/scr/script.js
@@ -0,0 +1,18 @@
+const schemeSvg = document.querySelector(".scheme-svg");
+const priceTotal = document.querySelector(".price-total");
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu");
+let cost = 800;
+let totalPrice = 0;
+schemeSvg.addEventListener("click", (event) => {
+  if (!event.target.classList.contains("booked")) {
+    event.target.classList.toggle("active");
+    let price = schemeSvg.querySelectorAll(".active").length;
+    totalPrice = price * cost;
+    priceTotal.textContent = totalPrice;
+  }
+});
+
+menuButton.addEventListener("click", () => {
+  menu.classList.toggle("is-open");
+});
